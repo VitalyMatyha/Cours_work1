3eec4d205203a2dfd5245a29bcc654bc6f9497f7
Vitaly_M <vitalymatyha@gmail.com>
2025-08-22 17:34:05 +0300

    add conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+# tests/conftest.py
+import pytest
+
+@pytest.fixture
+def sample_data():
+    return {"a": 1, "b": 2}
+
+@pytest.fixture(params=[1, 2, 3])
+def num(request):
+    return request.param
